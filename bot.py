438cf6bd6eb237aa3bd8b5f5e7b7477e0fc8f52c
Yadnyesh23 <yadnyeshhalde2006@gmail.com>
2025-03-03 16:13:11 +0530

    INITIAL

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 from pyrogram import Client, filters
 from pymongo import MongoClient
 import os
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 # Load from environment variables
 API_ID = int(os.getenv("API_ID"))
